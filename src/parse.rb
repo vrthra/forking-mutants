8b40492409eb9846c578aeccf299abfa6f63ee1e
Rahul Gopinath <gopinath@eecs.oregonstate.edu>
2015-05-24 05:30:33 -0700

    udpate

--- src/parse.rb
+++ src/parse.rb
@@ -1,6 +1,5 @@
 require 'ripper'
 require 'sorcerer'
-require 'ap'
 
 def transform(sexp)
   if sexp.class == Array
@@ -23,39 +22,6 @@ def match(sexp)
     return false
   end
 end
-
-# [:program,
-#   [:stmts_add,
-#     [:stmts_new],
-#     [:binary,
-#       [:vcall, [:@ident, "a", [1, 0]]],
-#       :"&&",
-#       [:vcall, [:@ident, "b", [1, 5]]]]
-#   ]
-# ]
-
-# [:program,
-#   [:stmts_add,
-#     [:stmts_new],
-#     [:method_add_arg,
-#       [:fcall, [:@ident, "mutate", [1, 0]]],
-#       [:arg_paren,
-#         [:args_add_block,
-#           [:args_add,
-#             [:args_add,
-#               [:args_add,
-#                 [:args_add, [:args_new], [:@int, "1", [1, 7]]],
-#                 [:vcall, [:@ident, "a", [1, 10]]]
-#               ],
-#               [:vcall, [:@ident, "b", [1, 13]]]
-#             ],
-#             [:dyna_symbol, [:xstring_add, [:xstring_new], [:@tstring_content, "&&", [1, 17]]]]
-#           ], false
-#         ]
-#       ]
-#     ]
-#   ]
-# ]
 
 $count = 1
 def mytransform(sexp)
@@ -83,7 +49,5 @@ end
 
 src = File.read('src/triangle.rb')
 sexp = Ripper::SexpBuilder.new(src).parse
-#sexp = Ripper::SexpBuilder.new("mutate(1, a, b,:'&&')").parse
-#sexp = transform(sexp)
 puts Sorcerer.source(transform(sexp), multiline: true, indent: true)
 
